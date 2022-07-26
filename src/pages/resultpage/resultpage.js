7c1602eaf564c8266dcdf24f231c7f33a663e1e0
MarcusBYUI <ojo18002@byui.edu>
2022-07-26 14:11:39 +0100

    added resultpage

--- src/pages/resultpage/resultpage.js
+++ src/pages/resultpage/resultpage.js
@@ -3,6 +3,8 @@ import Container from "../../components/container/container";
 import styles from "./resultpage.module.css";
 
 const Resultpage = (props) => {
+  const html =
+    "<p><span>industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </span></p><p><span>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop</span></p><p><span><span>&nbsp; &nbsp; industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop</span></span></p><p><span>&nbsp; &nbsp; industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with deskto</span></p><p>&nbsp;</p>";
   return (
     <Container>
       <div className={styles.grid}>
@@ -24,12 +26,23 @@ const Resultpage = (props) => {
           </p>
         </div>
       </div>
-      <h1>Lord Ganesha</h1>
-      <p>
-        {props.win
-          ? "Congratulations, <br/> you will be entered into the monthly drawing"
-          : `So sorry!, <br> try againi next month`}
-      </p>
+      <div className={styles.resultInfo}>
+        <h1>Lord Ganesha</h1>
+
+        {props.win ? (
+          <p className={styles.result}>
+            {" "}
+            Congratulations, <br /> you will be entered into the monthly drawing{" "}
+          </p>
+        ) : (
+          <p className={styles.result}>
+            {" "}
+            So sorry!, <br /> try againi next month{" "}
+          </p>
+        )}
+
+        <p dangerouslySetInnerHTML={{ __html: html }}></p>
+      </div>
     </Container>
   );
 };
